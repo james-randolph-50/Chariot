39421ac4ccd3b4ac688575f967250092aa2e342c
James <james.randolph2016@gmail.com>
2019-01-02 20:35:50 -0500

    cars reducer

--- chariot-client/src/store.js
+++ chariot-client/src/store.js
@@ -5,3 +5,15 @@ import {
 } from 'redux';
 
 import thunk from 'redux-thunk';
+
+const carsReducer = (state = [], action) => {
+    switch(action.type) {
+        case 'GET_CAR_SUCCESS';
+        return action.cars;
+
+        default:
+        return state;
+    }
+}
+
+const car = carsReducer();
