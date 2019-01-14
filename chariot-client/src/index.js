0b48360f93ae2d168534a460db6f801191b45aea
James <james.randolph2016@gmail.com>
2019-01-14 10:59:53 -0500

    add browser router

--- chariot-client/src/index.js
+++ chariot-client/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import store from './store.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
     <Provider store={store}>
