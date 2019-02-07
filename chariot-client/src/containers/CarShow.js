edf8564ea9196c6340cec9406ed7525da717a076
James <james.randolph2016@gmail.com>
2019-02-07 13:44:23 -0500

    adding to car show

--- chariot-client/src/containers/CarShow.js
+++ chariot-client/src/containers/CarShow.js
@@ -7,3 +7,32 @@ import CarCard from '../components/CarCard';
 import Cars from './Cars';
 import LikeButton from '../components/LikeButton';
 import { likeCar } from '../actions/carActions';
+
+class CarShow extends Component {
+
+    handleOnClick = () => {
+        this.props.likeCar(this.props.car[0])
+    }
+
+    componentDidMount() {
+        this.props.fetchCar(this.props.match.params.carId);
+    }
+
+    render() {
+        let car = this.props.car[0];
+        const {deleteCar, history} = this.props;
+
+        return (
+            <div className='carShow'>
+                {car ? (
+                    <div>
+                        <h1 className="carName">{car.name}</h1>
+                        <img src="{car.img_url}" style="max-width: 250px; max-height: 250px;"/>
+                        <h3>Cost: ${car.price}</h3>
+                        <h3>Year: {car.year}</h3>
+                )}
+            </div>
+
+        )
+    }
+}
