5c3c8aa7bf399dee0acfdf7c6155d528e9d2e16c
James <james.randolph2016@gmail.com>
2019-02-07 14:05:45 -0500

    add navbar component

---
+++ chariot-client/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Menu, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+      <div>
+        <Menu>
+          <Menu.Item header>Chariot</Menu.Item>
+          <Menu.Item as={Link} to='/'>
+            <Icon name="home"/>
+          </Menu.Item>
+          <Menu.Item as={Link} to='/cars' name='Cars'>
+          </Menu.Item>
+          <Menu.Item as={Link} to='/cars/new' name='Add A Car'>
+          </Menu.Item>
+          <Menu.Item as={Link} to='/about' name='About'>
+          </Menu.Item>
+          <Menu.Item as={Link} to='/contact' name='Contact Us'>
+            <Icon name="mail"/>
+          </Menu.Item>
+        </Menu>
+      </div>
+    );
+  }
+
+export default Navbar;
