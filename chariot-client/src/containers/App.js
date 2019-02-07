e3e26b7c68a3fb5f4ae62cf3ba77a9628823f505
James <james.randolph2016@gmail.com>
2019-02-07 14:03:16 -0500

    add router

--- chariot-client/src/containers/App.js
+++ chariot-client/src/containers/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import Cars from './Cars';
-import { getCars } from '../actions/cars';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Cars from './Cars';
+import CarForm from './components/CarForm';
+import CarShow from './components/CarShow';
+import About from './components/About';
+import Contact from './components/Contact';
+import { getCars } from '../actions/cars';
 
 
 class App extends Component {
@@ -9,7 +17,21 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <Cars />
+                <Router>
+                    <div>
+                        <Navbar/>
+                            <Container>
+                                <Switch>
+                                    <Route exact path='/' component={Home}/>
+                                    <Route exact path='/cars' component={Cars}/>
+                                    <Route exact path='/cars/new' component={CarForm}/>
+                                    <Route path='/cars/:carId' component={CarShow}/>
+                                    <Route exact path='/about' component={About}/>
+                                    <Route exact path='/contact' component={Contact}/>
+                                </Switch>
+                            </Container>
+                    </div>
+                </Router>
             </div>
         );
     }
