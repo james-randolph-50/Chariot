4982593786b766edee9483ddfb0eb529c693cdcc
James <james.randolph2016@gmail.com>
2019-03-06 14:05:16 -0500

    like button working

--- chariot-client/src/components/LikeButton.js
+++ chariot-client/src/components/LikeButton.js
@@ -3,7 +3,24 @@ import React from 'react';
 class LikeButton extends React.Component {
     constructor() {
         super();
+            this.state = {
+                liked: false
+            };
+            this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({
+            liked: !this.state.liked
+        });
+    }
+
+    render() {
+        const label = this.state.liked ? 'Unlike' : 'Like'
+            return (
+                <button className="like-btn" onClick={this.handleClick}>{label}</button>
+            );
     }
 }
 
-export default (LikeButton);
+export default LikeButton;
