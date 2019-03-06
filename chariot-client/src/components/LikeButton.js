714f9945b4f08d583fd43a0eb0cb25dfb880d327
James <james.randolph2016@gmail.com>
2019-03-06 13:17:27 -0500

    different approach to like button

--- chariot-client/src/components/LikeButton.js
+++ chariot-client/src/components/LikeButton.js
@@ -1,16 +1,8 @@
 import React from 'react';
 
 class LikeButton extends React.Component {
-
-    render() {
-
-      
-        return (
-            <div>
-                <button onClick={like()} className="like">Like</button>
-                <p>Likes: <span id="likes">0</span></p>
-            </div>
-        )
+    constructor() {
+        super();
     }
 }
 
